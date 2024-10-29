b6c41e7c58213d913b02bcc07b9b41beb95b033d
annkannn <144692934+annkannn@users.noreply.github.com>
2024-10-29 10:24:09 +0100

    Update Mangekant.py

--- Mangekant.py
+++ Mangekant.py
@@ -1,3 +1,18 @@
-class Monkus:
-    def __init__(self) -> None:
-        pass
+
+class kant:
+    def __init__(self,kanter, lengde):
+        self.kanter = kanter
+        self.sider = kanter
+        self.lengde = lengde
+    def omkrets(self):
+        return f"{self.kanter * self.lengde}"
+
+class kvadrat(kant):
+    def __init__(self):
+        super().__init__()
+    
+
+
+lengde = input("Hvor lang? ")        
+fourkatbnt = kant(4, lengde)
+print(fourkatbnt.lengde)
